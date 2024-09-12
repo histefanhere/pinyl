42c7d68c6c55893d6a72ff3d29fd2617be8e494a
histefanhere <stefan.zdra@gmail.com>
2024-09-13 00:19:39 +1200

    Basic combined behaviour

---
+++ main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from signal import pause
+from dotenv import load_dotenv
+import os
+from time import sleep
+from gpiozero import Button, TonalBuzzer
+from RPi.GPIO import cleanup
+from mfrc522 import SimpleMFRC522
+
+load_dotenv()
+
+reader = SimpleMFRC522()
+
+MODE = 'play'
+SAVE_DATA = ''
+
+def button_callback():
+    print("Button was pushed!")
+    
+    # playing = sp.current_user_playing_track()
+    # if playing['currently_playing_type'] != 'track':
+    #     print("Please play a track!")
+    #     return
+
+    # if playing['context'] == None:
+    #     print("ERROR: No context")
+    #     return
+
+    global MODE, SAVE_DATA
+    MODE = 'save'
+    # SAVE_DATA = playing['context']['uri']
+    
+    buzzer.play('B4')
+    sleep(0.2)
+    buzzer.stop()
+
+def main():
+    global MODE, SAVE_DATA
+    
+    global sp
+    scope = "user-read-playback-state,user-modify-playback-state"
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope, open_browser=False))
+    
+    global button
+    button = Button(os.getenv("BUTTON_PIN"))
+    button.when_pressed = button_callback
+    
+    global buzzer
+    buzzer = TonalBuzzer(os.getenv("BUZZER_PIN"))
+    
+    while True:
+        print("Waiting to scan...")
+        id, data = reader.read()
+        data = data.strip()
+        print(f"Found card with ID {id}, data: {data}")
+        
+        if MODE == 'play':
+            buzzer.play('C4')
+            sleep(0.2)
+            buzzer.stop()
+        
+            print("Playing data...")
+            if data.startswith('spotify'):
+                sp.start_playback(context_uri=data)
+            else:
+                print(f"Invalid card data \"{data}\"")
+
+        elif MODE == 'save':
+            print("Saving data...")
+            
+            playing = sp.current_user_playing_track()
+            if playing['currently_playing_type'] != 'track':
+                print("Please play a track!")
+                return
+            if playing['context'] == None:
+                print("ERROR: No context")
+                return
+            SAVE_DATA = playing['context']['uri']
+            
+            id, data = reader.write(SAVE_DATA)
+            print("Data saved!")
+            MODE = 'play'
+            
+            buzzer.play('C5')
+            sleep(0.2)
+            buzzer.stop()
+            sleep(5)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        cleanup()
+        print("Exiting...")
